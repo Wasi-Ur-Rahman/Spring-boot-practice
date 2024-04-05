a5859bcb716a0be76d27edad86fe1b3db520ea90
Wasi-Ur-Rahman <wasiurrahaman247@gmail.com>
2024-04-05 15:12:24 +0600

    added CRUD operation

--- src/main/java/org/spring/firstproject/topic/TopicController.java
+++ src/main/java/org/spring/firstproject/topic/TopicController.java
@@ -1,7 +1,7 @@
 package org.spring.firstproject.topic;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,12 +9,31 @@ import java.util.List;
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
-        return Arrays.asList(
-                new Topic("Spring", "Spring framerwork", "Spring framerwork description"),
-                new Topic("Java", "Java core", "Core java description"),
-                new Topic("javascript", "javascript framerwork", "javascript framerwork description")
-        );
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopicById(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method=RequestMethod.POST, value="/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value="/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(id, topic);
     }
 }
